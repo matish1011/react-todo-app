9846cffa4783b01c528ed2e89d13c965d07ed810
Matish Shrestha <matishshrestha@lftechnology.com>
2021-11-24 09:55:34 +0545

    Update the port from 5000 to 3000

--- cypress/integration/corinth/login.spec.js
+++ cypress/integration/corinth/login.spec.js
@@ -1,12 +1,9 @@
-/// < reference types="cypress" />
-
 describe('Login to the application', ()=>{
     
     it('first test', ()=>{
 
-        cy.visit("https://corinth.dev.app.nurixtx.net")
-        cy.get('delphi-corinth').shadow().find('global-css').shadow().find('slot').find('login-page').shadow().find('login-page-view').shadow().find('.container').shadow().get('[aria-labelledby="label"]').type('corinth-admin')
-        //cy.get('main').should('exists')
+        cy.visit("http://localhost:3000")
+        cy.title().should('eq', 'Todo App')
     })
 
 })
